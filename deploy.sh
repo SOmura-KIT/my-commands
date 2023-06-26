395a76af66685ab8787765514dafca4e51c687a1
Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-06-26 14:17:36 +0900

    add: binへの移動スクリプトの追加

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+# 実行可能スクリプトの配置先
+bin_path=${HOME}/.local/bin
+cp ./aur/aur    ${bin_path}
+cp ./pan/pan    ${bin_path}
+cp ./tef/tef    ${bin_path}
+cp ./vc/vc      ${bin_path}
+cp ./vcf/vcf    ${bin_path}
+cp ./vf/vf      ${bin_path}
+
+# sourceでシェルに読み込むスクリプトの配置先
+source_path=${HOME}/.local/source
+cp ./cdf/cdf.sh ${source_path}
