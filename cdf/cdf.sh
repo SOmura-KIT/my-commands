79d662ac53306d75ed9f51bc5c7c96b68b62d044
Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-06-29 15:27:24 +0900

    :refacter: コマンド説明のコメントを追加

--- cdf/cdf.sh
+++ cdf/cdf.sh
@@ -1,6 +1,11 @@
+# cdfコマンド (cdf)
+# カレントディレクトリ以下のディレクトリをfzfで選択して移動する
+
 cdf() {
     target_dir=$(find . -name "*" -type d 2>/dev/null | grep -v "\.git" | fzf)
     if [ -n "${target_dir}" ]; then
         cd ${target_dir}
     fi
 }
+
+# todo: 引数でディレクトリを指定できるようにする
