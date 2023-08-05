9b3f4810d89754944517f81218947804c6d2836c
Yuto Somura <c1100921@planet.kanazawa-it.ac.jp>
2023-08-05 22:53:19 +0900

    :add: カレントディレクトリを変更可能に

--- cdf/cdf.sh
+++ cdf/cdf.sh
@@ -2,10 +2,26 @@
 # カレントディレクトリ以下のディレクトリをfzfで選択して移動する
 
 cdf() {
+    # カレントディレクトリ
+    current_dir=$(pwd)
+
+    # 引数があればカレントディレクトリを変更
+    if [ $# -gt 0 ]; then
+        current_dir=$1
+    fi
+
+    # カレントディレクトリが存在しなければ終了
+    if [ ! -d ${current_dir} ]; then
+        echo "No such directory: ${current_dir}"
+        return 1
+    fi
+
+    # カレントディレクトリを移動
+    cd ${current_dir}
+
+    # カレントディレクトリ以下のディレクトリをfzfで選択
     target_dir=$(find . -name "*" -type d 2>/dev/null | grep -v "\.git" | fzf)
     if [ -n "${target_dir}" ]; then
         cd ${target_dir}
     fi
 }
-
-# todo: 引数でディレクトリを指定できるようにする
